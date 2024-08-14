8b7efc8d2f2915c0b1e9c27226fb9df38f3e58ba
LUCA DARIELLI <darielli.luca@gmail.com>
2024-08-14 00:17:29 -0300

    SanMiJs

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,38 +1,52 @@
 let losProductos = [
-    { id: 1, nombre: "Iphone 11", categoria: "Model-Old", stock: 2, precio: 700, rutaImagen: "iphone11b.png" },
-    { id: 2, nombre: "Iphone 12", categoria: "Model-Old", stock: 4, precio: 750, rutaImagen: "iphone12.png" },
-    { id: 3, nombre: "Iphone 13", categoria: "Model-Old", stock: 2, precio: 800, rutaImagen: "Iphone13.png" },
-    { id: 4, nombre: "Iphone 14", categoria: "Model-Normal", stock: 1, precio: 950, rutaImagen: "Iphone14.png" },
-    { id: 5, nombre: "Iphone 14 Pro", categoria: "Model-Normal", stock: 5, precio: 1100, rutaImagen: "Iphone14Pro.png" },
-    { id: 6, nombre: "Iphone 15", categoria: "Model-New", stock: 8, precio: 1500, rutaImagen: "Iphone15.png" },
-    { id: 7, nombre: "Iphone 15 Pro", categoria: "Model-New", stock: 8, precio: 1800, rutaImagen: "Iphone15prob.png" }]
+    { id: 1, nombre: "Iphone 11", categoria: "Model-Old", stock: 2, precio: 700, imagen: "iphone11b.png" },
+    { id: 2, nombre: "Iphone 12", categoria: "Model-Old", stock: 4, precio: 750, imagen: "iphone12.png" },
+    { id: 3, nombre: "Iphone 13", categoria: "Model-Old", stock: 2, precio: 800, imagen: "Iphone13.png" },
+    { id: 4, nombre: "Iphone 14", categoria: "Model-Normal", stock: 1, precio: 950, imagen: "Iphone14.png" },
+    { id: 5, nombre: "Iphone 14 Pro", categoria: "Model-Normal", stock: 5, precio: 1100, imagen: "Iphone14Pro.png" },
+    { id: 6, nombre: "Iphone 15", categoria: "Model-New", stock: 8, precio: 1500, imagen: "Iphone15.png" },
+    { id: 7, nombre: "Iphone 15 Pro", categoria: "Model-New", stock: 8, precio: 1800, imagen: "Iphone15prob.png" }]
 
 principal(losProductos)
 
 function principal(productos) {
-    let carrito= []
-    tarjetaspr(productos, carrito)
-
+    tarjetaspr(productos)
+    let carrito = obtenerCarrito()
+    renderizarCarrito(carrito)
 }
-function tarjetaspr(productos, carrito) {
+
+function obtenerCarrito() {
+    let carrito = []
+    if (localStorage.getItem("carrito")) {
+        carrito = JSON.parse(localStorage.getItem("carrito"))
+    }
+    return carrito
+}
+
+function setearCarrito(carrito) {
+    let carritoJSON = JSON.stringify(carrito)
+    localStorage.setItem("carrito", carritoJSON)
+}
+
+function tarjetaspr(productos) {
     let contenedorProductos = document.getElementById("productos")
     contenedorProductos.innerHTML = ""
-    productos.forEach(({ nombre, precio, stock, rutaImagen, id }) => {
+    productos.forEach(producto => {
         let tarjetaProducto = document.createElement("div")
         let clase = "producto"
 
         tarjetaProducto.className = clase
         tarjetaProducto.innerHTML = `
-                <h3>${nombre}</h3>
-                <img src=../assets/${rutaImagen}>
-                <p>$${precio}</p>
-                <p>Quedan ${stock} u.</p>
-                <button id=${id}>Añadir al carrito</button>
+                <h3>${producto.nombre}</h3>
+                <img src=../assets/${producto.imagen}>
+                <p>$${producto.precio}</p>
+                <p>Quedan ${producto.stock} u.</p>
+                <button id=${producto.id}>Añadir al carrito</button>
             `
         contenedorProductos.appendChild(tarjetaProducto)
 
-        let botonAgregarAlCarrito = document.getElementById(id)
-        botonAgregarAlCarrito.addEventListener("click", (e) => agregarAlCarrito(e, productos, carrito))
+        let botonAgregarAlCarrito = document.getElementById(producto.id)
+        botonAgregarAlCarrito.addEventListener("click", (e) => agregarAlCarrito(e, productos))
     })
 }
 let botonVerCatalago = document.getElementById("contacton2")
@@ -40,10 +54,13 @@ let main5 = document.getElementById("main5")
 let contenedorinicio = document.getElementById("inicio")
 let botonVolverinit = document.getElementById("init")
 let botoncarrito = document.getElementById("Carrito")
+let bproductos = document.getElementById("contacton3")
 
 
 botonVolverinit.addEventListener("click", mostrarEsconder2)
 botonVerCatalago.addEventListener("click", mostrarEsconder)
+botoncarrito.addEventListener("click",mostrarEsconder3 )
+bproductos.addEventListener("click", mostrarEsconder)
 
 function mostrarEsconder() {
     let contenedorinicio = document.getElementById("inicio")
@@ -53,6 +70,8 @@ function mostrarEsconder() {
     let bbuscador = document.getElementById("buscador")
     let bbuscar = document.getElementById("buscar")
     let lupita = document.getElementById("lupita")
+    let x = document.getElementById("x")
+    let bproductos = document.getElementById("contacton3")
     lupita.className = "mostrar2"
     contenedorinicio.className = "esconder"
     contenedorProductos.className = "mostrar"
@@ -60,6 +79,8 @@ function mostrarEsconder() {
     botonCarrito.className = "mostrar2"
     bbuscador.className = "mostrar2"
     bbuscar.className = "mostrar2"
+    x.className = "esconder"
+    bproductos.className = "esconder"
 }
 
 function mostrarEsconder2() {
@@ -69,14 +90,40 @@ function mostrarEsconder2() {
     let botonCarrito = document.getElementById("Carrito")
     let bbuscador = document.getElementById("buscador")
     let bbuscar = document.getElementById("buscar")
+    let x = document.getElementById("x")
+    let bproductos = document.getElementById("contacton3")
     contenedorinicio.className = "mostrar2"
     contenedorProductos.className = "esconder"
-        lupita.className = "esconder"
+    lupita.className = "esconder"
     botonInit.className = "esconder"
     botonCarrito.className = "esconder"
     bbuscador.className = "esconder"
     bbuscar.className = "esconder"
+    x.className = "esconder"
+    bproductos.className = "esconder"
+}
 
+
+function mostrarEsconder3(){
+    let contenedorinicio = document.getElementById("inicio")
+    let contenedorProductos = document.getElementById("productos")
+    let botonInit = document.getElementById("init")
+    let botonCarrito = document.getElementById("Carrito")
+    let bbuscador = document.getElementById("buscador")
+    let bbuscar = document.getElementById("buscar")
+    let botoncarrito = document.getElementById("Carrito")
+    let x = document.getElementById("x")
+    let bproductos = document.getElementById("contacton3")
+    contenedorinicio.className = "esconder"
+    contenedorProductos.className = "esconder"
+    lupita.className = "esconder"
+    botonInit.className = "mostrar2"
+    botonCarrito.className = "esconder"
+    bbuscador.className = "esconder"
+    bbuscar.className = "esconder"
+    botoncarrito.className = "esconder"
+    x.className = "mostrar2"
+    bproductos.className = "mostrar2"
 }
 
 
@@ -84,18 +131,43 @@ function mostrarEsconder2() {
 
 
 
-function agregarAlCarrito(e, productos, carrito){
-    let idProducto= e.target.id
-    let productoBuscados = productos.find(producto => producto.id === idProducto)
-    carrito.push({
-        id: productoBuscados.id,
-        nombre: productoBuscados.nombre,
-        precioUnitario: productoBuscados.precio,
-        unidades: 1,
-        subtotal: productoBuscados.precio
-    })
-
+function agregarAlCarrito(e, productos){
+    let carrito = obtenerCarrito()
+    let idProducto= Number(e.target.id)
+    let productoBuscado = productos.find(producto => producto.id === idProducto)
+        
+    let productoEnCarrito = carrito.find(producto => producto.id === idProducto);
+        
+    if (productoEnCarrito) {
+        productoEnCarrito.unidades += 1;
+        productoEnCarrito.subtotal = productoEnCarrito.precioUnitario * productoEnCarrito.unidades;
+    } else {
+        carrito.push({
+            id: productoBuscado.id,
+            nombre: productoBuscado.nombre,
+            precioUnitario: productoBuscado.precio,
+            unidades: 1,
+            subtotal: productoBuscado.precio
+        });
+    }    
+    setearCarrito(carrito);        
+    renderizarCarrito(carrito);        
+    console.log("Carrito después de añadir:", carrito);
 }
+
+function renderizarCarrito(carrito) {
+    let contenedorCarrito = document.getElementById("contenedorCarrito")
+    contenedorCarrito.innerHTML = ""
+    carrito.forEach(productoBuscado=> {
+        contenedorCarrito.innerHTML += `
+                <p>Producto elegido: ${productoBuscado.nombre}</p>
+                <p>Precio Unitario: ${productoBuscado.precioUnitario}</p>
+                <p>Unidades seleccionadas: ${productoBuscado.unidades}</p>
+                <p>Subtotal: ${productoBuscado.subtotal}</p>
+        `
+    })}
+
+
 
     let input = document.getElementById("buscador")
     let botonbuscar = document.getElementById("buscar")
@@ -108,3 +180,6 @@ function agregarAlCarrito(e, productos, carrito){
         let productosFiltrados = productos.filter(producto => producto.nombre.includes(valorStr) || producto.precio.toString().includes(valorStr))
         tarjetaspr(productosFiltrados)
     }
+
+
+
